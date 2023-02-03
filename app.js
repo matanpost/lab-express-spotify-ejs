b2978e9c8eb723d972c6f94902976253d7a97bd5
Matan Post <matanpost@gmail.com>
2023-02-03 17:35:43 +0100

    iteration 3

--- app.js
+++ app.js
@@ -26,5 +26,26 @@ app.use(express.static(__dirname + '/public'))
 // setting the spotify-api goes here:
 
 // Our routes go here:
+app.get('/', (req, res) => {
+    res.render('index')
+  })
+
+app.get('/artist-search', (req, res) => {
+    console.log(req.query)
+    spotifyApi
+  .searchArtists(req.query.searchTerm)
+  .then(data => {
+    console.log('The received data from the API: ', data.body.artists.items)
+    // ----> 'HERE WHAT WE WANT TO DO AFTER RECEIVING THE DATA FROM THE API'
+   //res.send(data.body.artists.items)
+   const artistArray = data.body.artists.items
+   console.log(artistArray[0].images[0]);
+   res.render("artist-search-results", {artistArray})
+  })
+  .catch(err => console.log('The error while searching artists occurred: ', err))
+  
+ })
+
+
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'))
